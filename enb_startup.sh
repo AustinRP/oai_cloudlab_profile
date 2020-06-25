52246cf2c41133ca22a7b22069f563a925dd0f14
Austin Pahl <austinpahl@gmail.com>
2020-06-24 23:52:41 -0600

    Debug more

--- enb_startup.sh
+++ enb_startup.sh
@@ -5,10 +5,12 @@ exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>/tmp/startup_log.out 2>&1
 
-source /local/repository/shared_startup.sh
+echo test
 
-cd /mydata/openairinterface5g
-source oaienv
-cd cmake_targets/
-./build_oai -I -w USRP --eNB
-
+#source /local/repository/shared_startup.sh
+#
+#cd /mydata/openairinterface5g
+#source oaienv
+#cd cmake_targets/
+#./build_oai -I -w USRP --eNB
+#
--- ue_startup.sh
+++ ue_startup.sh
@@ -4,10 +4,11 @@ set -ex
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>/mydata/startup_log.out 2>&1
-
-source /local/repository/shared_startup.sh
-
-cd /mydata/openairinterface5g
-source oaienv
-cd cmake_targets/
-./build_oai -I -w USRP --UE
+echo test
+#
+#source /local/repository/shared_startup.sh
+#
+#cd /mydata/openairinterface5g
+#source oaienv
+#cd cmake_targets/
+#./build_oai -I -w USRP --UE
