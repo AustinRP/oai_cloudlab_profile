522598f25b456d96203e79ba1fdb1d66230debc2
Austin Pahl <austinpahl@gmail.com>
2020-06-24 22:23:43 -0600

    Add logging

--- shared_startup.sh
+++ shared_startup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 set -e
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>/tmp/startup_log.out 2>&1
 
 git clone --branch v1.2.1 https://gitlab.eurecom.fr/oai/openairinterface5g.git /mydata/openairinterface5g
 
